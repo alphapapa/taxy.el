d933a9633a4d3fcba8a212a14f61790b14e0f7ab
Adam Porter <adam@alphapapa.net>
2021-09-26 07:46:23 +0000

    WIP
    Works fairly well, but items always appear after sub-taxys, even
    though they are inserted first in the code.
    
    Also, it seems impossible to recursively get the node at point in
    inferior EWOCs, but maybe I'm doing something wrong.
    
    And there's excessive spacing between some subsections, which is
    inserted by EWOC itself.  That might be fixable.

--- taxy-ewoc.el
+++ taxy-ewoc.el
@@ -27,10 +27,19 @@
 (require 'cl-lib)
 (require 'ewoc)
 
+(defgroup taxy-ewoc nil
+  "Display Taxy structs with EWOC."
+  :group 'taxy)
+
 ;;;; Variables
 
 (defvar taxy-ewoc-depth 0)
 (defvar taxy-ewoc-level-indent 2)
+(defvar taxy-ewoc-nosep nil)
+(defvar taxy-ewoc-parent-ewoc nil)
+(defvar taxy-ewoc-footers nil)
+
+(defvar-local taxy-ewoc-root-ewoc nil)
 
 ;;;; Faces
 
@@ -42,27 +51,19 @@
   '((t :inherit diff-file-header))
   "Taxy descriptions in EWOC headers.")
 
+;;;; Commands
+
+(cl-defun taxy-ewoc-toggle (node (taxy-ewoc-locate taxy-ewoc-root-ewoc)))
+
 ;;;; Functions
 
-(cl-defun taxy-ewoc-insert (taxy &key nosep footer
-				 (header "HEADER"))
-  "Return an EWOC displaying TAXY at point in current buffer.
-HEADER, FOOTER, and NOSEP are passed to `ewoc-create', which
-see."
-  (let* ((indent (make-string (* taxy-ewoc-level-indent
-				 taxy-ewoc-depth)
-			      ? ))
-         (header (concat indent
-                         (propertize (or (taxy-name taxy) header)
-                                     'face 'taxy-ewoc-name)
-		         (when (taxy-description taxy)
-			   (concat ": " (propertize (taxy-description taxy)
-                                                    'face 'taxy-ewoc-description)))))
-         (ewoc (ewoc-create #'taxy-ewoc--pretty-print
-			    header footer nosep))
-	 (taxy-ewoc-depth (1+ taxy-ewoc-depth)))
-    (ewoc-enter-first ewoc taxy)
-    ewoc))
+(cl-defun taxy-ewoc-locate (ewoc &optional (pos (point)))
+  "Return node at POS in EWOC.
+Looks in child EWOCs."
+  (when-let ((node (ewoc-locate ewoc pos)))
+    (cl-typecase (ewoc-data node)
+      (ewoc (taxy-ewoc-locate (ewoc-data node) pos))
+      (t node))))
 
 (cl-defgeneric taxy-ewoc--pretty-print (thing)
   (insert (make-string (* taxy-ewoc-level-indent taxy-ewoc-depth)
@@ -70,8 +71,45 @@ see."
   (prin1 thing (current-buffer)))
 
 (cl-defmethod taxy-ewoc--pretty-print ((taxy taxy))
-  (mapc #'taxy-ewoc--pretty-print (taxy-items taxy))
-  (mapc #'taxy-ewoc-insert (taxy-taxys taxy)))
+  (dolist (item (taxy-items taxy))
+    (ewoc-enter-last taxy-ewoc-parent-ewoc item))
+  (dolist (sub-taxy (taxy-taxys taxy))
+    (let* ((indent (make-string (* taxy-ewoc-level-indent
+				   taxy-ewoc-depth)
+			        ? ))
+           (header (concat indent (taxy-ewoc-header sub-taxy)))
+           (footer (concat indent (taxy-ewoc-footer sub-taxy)))
+           (new-ewoc (ewoc-create #'taxy-ewoc--pretty-print
+                                  header footer taxy-ewoc-nosep))
+	   (taxy-ewoc-depth (1+ taxy-ewoc-depth)))
+      (ewoc-enter-last taxy-ewoc-parent-ewoc new-ewoc)
+      (let ((taxy-ewoc-parent-ewoc new-ewoc))
+        (ewoc-enter-last new-ewoc sub-taxy)))))
+
+(cl-defmethod taxy-ewoc--pretty-print ((ewoc ewoc))
+  (let ((taxy-ewoc-parent-ewoc ewoc))
+    (taxy-ewoc--pretty-print (ewoc-data ewoc))))
+
+(cl-defmethod taxy-ewoc--pretty-print ((_function function))
+  ;; HACK: Not sure why this is being called for the EWOC printer function itself, but
+  ;; this prevents it from being printed.
+  nil)
+
+(defun taxy-ewoc-header (taxy)
+  "Return header string for TAXY."
+  (concat (propertize (or (taxy-name taxy) "")
+		      'face 'taxy-ewoc-name)
+	  (when (taxy-description taxy)
+	    (concat ": " (propertize (taxy-description taxy)
+				     'face 'taxy-ewoc-description)))))
+
+(defun taxy-ewoc-footer (taxy)
+  "Return footer string for TAXY."
+  (when taxy-ewoc-footers
+    (concat "FOOTER: " (taxy-name taxy)
+            (when (taxy-description taxy)
+	      (concat ": " (propertize (taxy-description taxy)
+                                       'face 'taxy-ewoc-description))))))
 
 ;;;; Footer
 
