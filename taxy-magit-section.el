250211dcc90e47c12323c6b0af22dd74d34b34fa
Adam Porter <adam@alphapapa.net>
2022-03-29 00:27:56 -0500

    Release: 0.9

--- taxy-magit-section.el
+++ taxy-magit-section.el
@@ -5,7 +5,7 @@
 ;; Author: Adam Porter <adam@alphapapa.net>
 ;; Maintainer: Adam Porter <adam@alphapapa.net>
 ;; URL: https://github.com/alphapapa/taxy.el
-;; Version: 0.9-pre
+;; Version: 0.9
 ;; Package-Requires: ((emacs "26.3") (magit-section "3.2.1"))
 ;; Keywords: lisp
 
