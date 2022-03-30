def197039d2d4ba9b36045badc0d4a1756fcbb1c
Adam Porter <adam@alphapapa.net>
2022-03-30 00:45:43 -0500

    WIP: Almost works
    It actually almost works, with the use case of ement-taxy-room-list in
    its wip/taxy-nested-spaces branch.  Current problem is that some group
    hierarchies are duplicated, rather than the correct group hierarchy
    being detected for each item.  This may actually be workable, though.

--- taxy.el
+++ taxy.el
@@ -238,6 +238,134 @@ numbers greater-than-or-equal-to 10 are tested against
            ;; other chains, or push to this taxy if they don't take it.
            (offer-or-push))))))
 
+(cl-defun taxy-take-keyed-nested*
+    (key-fns item taxy
+             &key (key-name-fn #'identity) (then #'ignore))
+  "Take ITEM into TAXY, adding new taxys dynamically and recursively.
+Places ITEM into a taxy in TAXY for the value returned by
+KEY-FNS called with ITEM.  The new taxys are added to TAXY
+recursively as necessary.  Each new taxy's name is that returned
+by KEY-NAME-FN called with ITEM.
+
+Each element of KEY-FNS may be a function or a list of functions.
+A list of functions creates a \"chain\" of functions: when an
+item is matched by the first function in a chain, it is placed
+in that chain's taxonomy, and is not \"offered\" to functions
+outside of that chain.
+
+For example, if KEY-FNS were:
+
+  '(((lambda (n) (< n 10)) oddp)
+    ((lambda (n) (>= n 10)) evenp))
+
+Then a list of numbers from 0-19 would be classified
+like (listing numbers on a single line for the sake of example):
+
+  - <10:
+    - 0, 2, 4, 6, 8
+    - oddp:
+      - 1, 3, 5, 7, 9
+  - >=10:
+    - 11, 13, 15, 17, 19
+    - evenp:
+      - 10, 12, 14, 16, 18
+
+So only numbers below 10 are tested against `oddp', and only
+numbers greater-than-or-equal-to 10 are tested against
+`evenp'.  (A contrived example, of course, since testing against
+`evenp' or `oddp' is just the inverse.)"
+  (declare (indent defun))
+  (cl-macrolet ((offer-or-push
+                 () `(if (cdr key-fns)
+                         (taxy-take-keyed-nested* (cdr key-fns) item taxy
+                           :key-name-fn key-name-fn :then then)
+                       (push item (taxy-items taxy)))))
+    (cl-typecase (car key-fns)
+      (function
+       ;; A single key function.
+       (let ((key-fn (car key-fns))
+             item-taxy)
+         (if-let ((key (funcall key-fn item)))
+             ;; This key function returned non-nil for the item:
+             ;; apply it to the appropriate sub-taxy.
+             (let ((key-taxy
+                    (or (cl-find-if (lambda (taxy-key)
+                                      (equal key taxy-key))
+                                    (taxy-taxys taxy)
+                                    :key #'taxy-key)
+                        ;; No existing, matching sub-taxy found: make
+                        ;; a new one and add it to TAXY's sub-taxys.
+                        (cl-typecase key
+                          (atom
+                           ;; A single key: make and add a single taxy.
+                           (car
+                            (push (funcall
+                                   ;; NOTE: Calling `make-taxy' directly might offer the
+                                   ;; compiler a chance to optimize compared to using `funcall',
+                                   ;; but allowing taxy structs to specify their own MAKE
+                                   ;; functions is very helpful when using specialized structs.
+                                   (taxy-make taxy)
+                                   :name (funcall key-name-fn key)
+                                   :key key
+                                   :predicate (lambda (item)
+                                                (equal key (funcall key-fn item)))
+                                   :take (when (cdr key-fns)
+                                           (lambda (item taxy)
+                                             (taxy-take-keyed (cdr key-fns) item taxy
+                                               :key-name-fn key-name-fn :then then)))
+                                   :then then)
+                                  (taxy-taxys taxy))))
+                          (list
+                           ;; A list of keys: make and add a hierarchy of taxys.
+                           (cl-labels ((new-taxy
+                                        () (funcall
+                                            ;; NOTE: Calling `make-taxy' directly might offer the
+                                            ;; compiler a chance to optimize compared to using `funcall',
+                                            ;; but allowing taxy structs to specify their own MAKE
+                                            ;; functions is very helpful when using specialized structs.
+                                            (taxy-make taxy)
+                                            :name (funcall key-name-fn (car key))
+                                            :key (car key)
+                                            :predicate (lambda (item)
+                                                         (equal (car key) (funcall key-fn item)))
+                                            :take (when (cdr key-fns)
+                                                    (lambda (item taxy)
+                                                      (taxy-take-keyed (cdr key-fns) item taxy
+                                                        :key-name-fn key-name-fn :then then)))
+                                            :then then)))
+                             (let (top-taxy previous-taxy new-taxy)
+                               (setf top-taxy (new-taxy)
+                                     new-taxy top-taxy
+                                     previous-taxy top-taxy
+                                     key (cdr key))
+                               (while key
+                                 (setf new-taxy (new-taxy))
+                                 (push new-taxy (taxy-taxys previous-taxy))
+                                 (setf previous-taxy new-taxy
+                                       item-taxy new-taxy
+                                       key (cdr key)))
+                               (car
+                                (push top-taxy (taxy-taxys taxy))))))))))
+               (if (cdr key-fns)
+                   ;; Other key-fns remain: offer item to them, allowing
+                   ;; them to create more sub-taxys beneath this key-taxy.
+                   (taxy-take-keyed-nested* (cdr key-fns) item key-taxy
+                     :key-name-fn key-name-fn :then then)
+                 ;; No more key-fns remain: add item to this taxy.
+                 (push item (taxy-items (or item-taxy key-taxy)))))
+           ;; No key value: offer to other KEY-FNS or push to this taxy.
+           (offer-or-push))))
+      (list
+       ;; A "chain" of key functions.
+       (or (when (funcall (caar key-fns) item)
+             ;; The first function in this chain returns non-nil for
+             ;; the item: apply the item to the chain.
+             (taxy-take-keyed-nested* (car key-fns) item taxy
+               :key-name-fn key-name-fn :then then))
+           ;; This "chain" of key-fns didn't take the item: offer it to
+           ;; other chains, or push to this taxy if they don't take it.
+           (offer-or-push))))))
+
 (defun taxy-size (taxy)
   "Return the number of items TAXY holds.
 Includes items in TAXY's sub-taxys."
@@ -357,6 +485,52 @@ defined with a definer defined by `taxy-define-key-definer')."
       (setf keys (mapcar #'quote-fn keys))
       `(lambda (item taxy)
          (taxy-take-keyed ',keys item taxy)))))
+
+(defun taxy-make-take-function-nested* (keys aliases)
+  "Return a `taxy' \"take\" function for KEYS.
+Each of KEYS should be a function alias defined in ALIASES, or a
+list of such KEY-FNS (recursively, ad infinitum, approximately).
+ALIASES should be an alist mapping aliases to functions (such as
+defined with a definer defined by `taxy-define-key-definer')."
+  (let ((macrolets (cl-loop for (name . fn) in aliases
+                            collect `(,name ',fn))))
+    (cl-labels ((expand-form
+                 ;; Is using (cadr (macroexpand-all ...)) really better than `eval'?
+                 (form) (cadr (macroexpand-all
+                               `(cl-symbol-macrolet (,@macrolets)
+                                  ,form))))
+                (quote-fn
+                 (fn) (pcase fn
+                        ((pred symbolp) (expand-form fn))
+                        (`(,(and (or 'and 'or 'not) boolean)
+                           . ,(and args (map (:name name) (:keys keys))))
+                         ;; Well, that pcase expression isn't confusing at all...  ;)
+                         ;;  (cl-assert name t "Boolean key functions require a NAME")
+                         ;;  (cl-assert keys t "Boolean key functions require KEYS")
+                         `(lambda (buffer)
+                            (when (cl-loop for fn in ',(mapcar #'quote-fn (or keys args))
+                                           ,(pcase boolean
+                                              ('and 'always)
+                                              ('or 'thereis)
+                                              ('not 'never))
+                                           (funcall fn buffer))
+                              (or ,name ""))))
+                        (`(,(and (pred symbolp) fn)
+                           . ,(and args (guard (pcase (car args)
+                                                 ((or (pred keywordp)
+                                                      (and (pred atom)
+                                                           ;; SOMEDAY: Use (not symbolp) when depending on Emacs 28.1.
+                                                           (pred (lambda (it) (not (symbolp it)))))
+                                                      `(quote ,_))
+                                                  t)))))
+                         ;; Key with args: replace with a lambda that
+                         ;; calls that key's function with given args.
+                         `(lambda (element)
+                            (,(expand-form fn) element ,@args)))
+                        ((pred listp) (mapcar #'quote-fn fn)))))
+      (setf keys (mapcar #'quote-fn keys))
+      `(lambda (item taxy)
+         (taxy-take-keyed-nested* ',keys item taxy)))))
 
 ;;;; Documentation group
 
