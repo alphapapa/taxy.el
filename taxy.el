8b66f800aa9ddddeff7c38f2857102e1f1e469bc
Adam Porter <adam@alphapapa.net>
2025-11-29 05:36:50 -0600

    Add: (taxy-lift)
    The first new feature in a long time!

--- taxy.el
+++ taxy.el
@@ -119,6 +119,31 @@ when reusing taxy definitions."
           (cl-loop for taxy in (taxy-taxys taxy)
                    append (taxy-flatten taxy))))
 
+(defvar taxy-lift-min-items 2
+  "In `taxy-lift', taxys with fewer than this many items will be \"lifted\".
+That is, their items will be lifted into their parent taxy, and the taxy
+itself will be removed from the parent taxy's taxys.
+
+This is defined as a special variable because passing the argument
+directly to the function would deny the ability to call the function in
+a threading style.")
+
+(defun taxy-lift (taxy)
+  "Return TAXY having \"lifted\" its sub-taxys.
+That is, a sub-taxy which has fewer items than `taxy-lift-min-items'
+will have its items \"lifted\" into its parent taxy's items, and the
+sub-taxy itself will be removed from its parent taxy's taxys.  This
+operates recursively within TAXY's sub-taxys."
+  (cl-labels ((rec (taxy)
+                (mapc #'rec (taxy-taxys taxy))
+                (dolist (sub-taxy (taxy-taxys taxy))
+                  (when (< 0 (length (taxy-items sub-taxy)) taxy-lift-min-items)
+                    (cl-callf2 append (taxy-items sub-taxy) (taxy-items taxy))
+                    (cl-callf2 remq sub-taxy (taxy-taxys taxy))
+                    (setf (taxy-items sub-taxy) nil)))))
+    (rec taxy))
+  taxy)
+
 (defun taxy-mapcar-items (fn taxy)
   "Return copy of TAXY, having replaced its items with the value of FN on each.
 Replaces every item in TAXY and its descendants.  Useful to
